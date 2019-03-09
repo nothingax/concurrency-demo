84b985bc09c653b336b0575dd89358937d8ca924
zhangjianwei <CSSVDD@gmail.com>
2019-03-09 20:51:31 +0800

    添加几个用于标识作用的注解

---
+++ src/main/java/com/example/concurrencydemo/annotation/NotThreadSafe.java
@@ -0,0 +1,22 @@
+package com.example.concurrencydemo.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Program Name: concurrency-demo
+ * <p>
+ * Description: 【线程不安全】仅仅标识作用
+ * <p>
+ *
+ * @author zhangjianwei
+ * @version 1.0
+ * @date 2019/3/9 7:49 PM
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
